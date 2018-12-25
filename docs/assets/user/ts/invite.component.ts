cf9eac8cd959904a68ee0734d13aed857d45de14
Andrew Oplas <andrewmoplas@gmail.com>
2018-12-25 20:39:26 +0800

    [Task/#0012] - ng build

---
+++ docs/assets/user/ts/invite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite',
+  templateUrl: '../html/invite.component.html',
+  styleUrls: ['../scss/invite.component.scss']
+})
+export class InviteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
