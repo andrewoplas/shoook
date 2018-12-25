a65916108790154b8b650bf241a1649b1a655726
Andrew Oplas <andrewmoplas@gmail.com>
2018-12-25 19:29:08 +0800

    [Task/#0012] - Added and modified footer files

---
+++ src/assets/vendor/ts/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vendor-footer',
+  templateUrl: '../html/footer.component.html',
+  styleUrls: ['../scss/footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
