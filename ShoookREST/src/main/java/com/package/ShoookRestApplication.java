8885c04b6c7c1fc12bc277bbf120aa593eda91a8
Andrew Oplas <andrewmoplas@gmail.com>
2019-01-01 09:05:23 +0800

    [Task/#0016] - Finalized REST framework

---
+++ ShoookREST/src/main/java/com/package/ShoookRestApplication.java
@@ -0,0 +1,14 @@
+package com.package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoookRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoookRestApplication.class, args);
+	}
+
+}
+
