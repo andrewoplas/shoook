0d0ddaf48e5968eeb045a0abadee6b4737aa5d61
Andrew Oplas <andrewmoplas@gmail.com>
2019-02-02 20:38:34 +0800

    [Task/#0028] - Fixed some bugs

--- src/app/modules/user/landing/landing.component.ts
+++ src/app/modules/user/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,13 +9,35 @@ import * as $ from 'jquery';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  public forms = this.formBuilder.group({
+    occassion: [""],
+    date: [""],
+    time: [""],
+    location: [""],
+    numberOfGuest: [""]
+  });
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
-  ngOnInit() { 
+  public ngOnInit() { 
     eval("$('#testimonial').slick({slidesToShow: 3,slidesToScroll: 1,autoplay: true,autoplaySpeed: 2000,dots: true})");
     eval("$('#txtTime').timepicki();");
     eval("$('#txtDate').datepicker({minDate: new Date(),language: 'en',dateFormat: 'mm/dd/yyyy'});");
   }
-  
+
+  public search() {
+    let search = {
+      occassion: this.forms.value.occassion.trim(),
+      date: this.forms.value.date.trim(),
+      time: this.forms.value.time.trim(),
+      location: this.forms.value.location.trim(),
+      guests: this.forms.value.numberOfGuest.trim(),
+    }
+
+    this.router.navigate(['/search', search]);
+  }
+
 }
--- src/app/modules/user/landing/landing.module.ts
+++ src/app/modules/user/landing/landing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { SharedModule } from '@shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { SharedModule } from '@shared/shared.module';
   ],
   imports: [
     SharedModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LandingModule { }
